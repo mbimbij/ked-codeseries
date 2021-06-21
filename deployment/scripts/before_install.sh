fe1d8cd11b55b24b3c7873dd4abb0c4df127d6e2
mbimbij <joseph.mbimbi@gmail.com>
2021-06-21 18:37:34 +0200

    Add deployment related files and remove some other useless and leftovers files

---
+++ deployment/scripts/before_install.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# making the application working directory and configuration at the same time
+mkdir -p /opt/application/conf
---
+++ deployment/scripts/stop_server.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+systemctl stop application || echo 0
