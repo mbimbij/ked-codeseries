2587220d326e024fe5e9225c37496edd4ffcbfb7
mbimbij <joseph.mbimbi@gmail.com>
2021-06-21 16:53:48 +0200

    Replace `click-count` application with a web "hello world"

--- src/main/java/org/example/SpringBootApplicationRunner.java
+++ src/main/java/org/example/SpringBootApplicationRunner.java
@@ -1,11 +1,19 @@
-package fr.xebia.clickcount;
+package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootApplicationRunner {
   public static void main(String[] args) {
     SpringApplication.run(SpringBootApplicationRunner.class, args);
+  }
+
+  @GetMapping("/")
+  public String helloWorld(){
+    return "Hello World !";
   }
 }
