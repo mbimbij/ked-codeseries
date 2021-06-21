cc227bfe0cbd2bdc5b7622d669fb1251792b1811
mbimbij <joseph.mbimbi@gmail.com>
2021-06-21 19:08:26 +0200

    Add a uuid to application instance and to the REST response, to differentiate instances in LB response

--- src/main/java/org/example/SpringBootApplicationRunner.java
+++ src/main/java/org/example/SpringBootApplicationRunner.java
@@ -5,15 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @SpringBootApplication
 @RestController
 public class SpringBootApplicationRunner {
+
+  public static final String uuid = UUID.randomUUID().toString();
+
   public static void main(String[] args) {
     SpringApplication.run(SpringBootApplicationRunner.class, args);
   }
 
   @GetMapping("/")
   public String helloWorld(){
-    return "Hello World !";
+    return "Hello World ! :: "+uuid;
   }
 }
